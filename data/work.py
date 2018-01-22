b5b999105e1e189ddd527d350af26168847d98b3
grandfunkygamer <rick.canon@yahoo.com>
2018-01-22 22:41:40 +0000

    Jinja 101

--- data/work.py
+++ data/work.py
@@ -5,12 +5,21 @@ WORK = [
     "title": "THE BABY HUB",
     "type": ("""E-commerce"""),
     "id": "thebabyhub",
-
+    },
+    {
 
     "title": "NATE BATHROOMS",
     "type": ("""E-commerce"""),
     "id": "natebathrooms",
+},
+{
 
+    "title": "CITSYS",
+    "type": ("""Website"""),
+    "id": "citsys",
+},
+
+{
 
     "title": "CITSYS",
     "type": ("""Website"""),
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, redirect
-
 from google.appengine.ext import ndb
+from flask import Flask, render_template, request, redirect
+from data import WORK
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', work_list=WORK)
-
-
-from data import WORK
-
+	print(WORK)
+	return render_template('index.html', work_list=WORK)

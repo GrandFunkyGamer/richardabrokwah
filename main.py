132288f4d7e498a132542fc4bcca92f934cb0bb6
grandfunkygamer <rick.canon@yahoo.com>
2019-03-26 23:59:02 +0100

    define global name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from google.appengine.ext import ndb
 from flask import Flask, render_template, request, redirect
 from data import WORK
+from data import BLOG
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-	print(WORK)
-	return render_template('index.html', work_list=WORK)
+	return render_template('index.html', work_list=WORK, blog_list=BLOG)

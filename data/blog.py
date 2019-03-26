15261e5ff52a2899ce7893172aea8b5082d85d64
grandfunkygamer <rick.canon@yahoo.com>
2019-03-27 00:02:37 +0100

    ....

---
+++ data/blog.py
@@ -0,0 +1,20 @@
+BLOG = [
+
+{
+	"image": "blog1.jpg",
+    "title": ("""The Sticker removal madness <br> &nbsp;"""),
+    "short_desc": ("""I love stickers, i put them on my laptop, on my guitar, my car and basically anything else it could get a grip on."""),
+    "url": ("""https://medium.com/@grandfunkygamer/the-sticker-removal-madness-2f38a956476b"""),
+    "id": 1,
+},
+
+{
+	"image": "blog2.jpg",
+    "title": "Uploading sitemap.xml to google app engine",
+    "short_desc": ("""Ever wondered how you can get your site in the results of Google searches. Well i did too and found out that in order to achieve that I had to submit a sitemap to Google."""),
+    "url": ("""https://medium.com/@grandfunkygamer/uploading-sitemap-xml-to-google-app-engine-cc14597797c7"""),
+    "id": 2,
+},
+
+
+]

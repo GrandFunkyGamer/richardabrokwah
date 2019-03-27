76a63d3a514008cbb8d08971c9225b9a3966782f
grandfunkygamer <rick.canon@yahoo.com>
2019-03-27 21:16:29 +0100

    blog data

--- data/blog.py
+++ data/blog.py
@@ -2,6 +2,7 @@ BLOG = [
 
 {
 	"image": "blog1.jpg",
+    "alt": "Laptop with stickers",
     "title": ("""The Sticker removal madness <br> &nbsp;"""),
     "short_desc": ("""I love stickers, i put them on my laptop, on my guitar, my car and basically anything else it could get a grip on."""),
     "url": ("""https://medium.com/@grandfunkygamer/the-sticker-removal-madness-2f38a956476b"""),
@@ -10,10 +11,20 @@ BLOG = [
 
 {
 	"image": "blog2.jpg",
+    "alt": "Code Blocks",
     "title": "Uploading sitemap.xml to google app engine",
     "short_desc": ("""Ever wondered how you can get your site in the results of Google searches. Well i did too and found out that in order to achieve that I had to submit a sitemap to Google."""),
     "url": ("""https://medium.com/@grandfunkygamer/uploading-sitemap-xml-to-google-app-engine-cc14597797c7"""),
     "id": 2,
+},
+
+{
+    "image": "blog3.jpg",
+    "alt": "Desk with journal and coffee",
+    "title": "Embedding Code in Medium <br> &nbsp;",
+    "short_desc": ("""Wondering how to embed code or code-block in your medium post ? Well, that's no cause for a he"""),
+    "url": ("""https://medium.com/@grandfunkygamer/embedding-code-in-medium-68156a0013ed"""),
+    "id": 3,
 },
 
 
